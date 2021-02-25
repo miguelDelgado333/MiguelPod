f7735eb60ad3c72ff1709628433293d0236217d9
Miguel Delgado <miguel.delgado@globant.com>
2021-02-25 16:53:12 -0600

    Initial Commit

---
+++ MiguelPod/MiguelPod.h
@@ -0,0 +1,18 @@
+//
+//  MiguelPod.h
+//  MiguelPod
+//
+//  Created by Miguel Angel Delgado Alcantara on 25/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MiguelPod.
+FOUNDATION_EXPORT double MiguelPodVersionNumber;
+
+//! Project version string for MiguelPod.
+FOUNDATION_EXPORT const unsigned char MiguelPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MiguelPod/PublicHeader.h>
+
+
